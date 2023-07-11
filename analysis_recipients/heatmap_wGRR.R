5f45964c4d75e095ac2b9e7c7ccef1e849e9939c
Laura Toribio-Celestino <105511747+LaboraTORIbio@users.noreply.github.com>
2023-07-11 17:15:03 +0200

    Update heatmap_wGRR.R

--- analysis_recipients/heatmap_wGRR.R
+++ analysis_recipients/heatmap_wGRR.R
@@ -2,8 +2,8 @@ library(pheatmap)
 library(RColorBrewer)
 library(tidyr)
 
-order <- c("EC19", "EC22", "EC12", "EC07", "EC02", "EC03", "EC10", "EC21", "EC16", "EC25",
-              "KPN10", "KPN14", "KPN01", "KPN16", "KQ01", "KPN09", "KPN12", "KPN02", "KPN11", "KQ04")
+order <- c("EC22", "EC12", "EC07", "EC02", "EC19", "EC03", "EC10", "EC21", "EC16", "EC25",
+           "KPN10", "KPN01", "KPN14", "KPN16", "KQ01", "KPN09", "KPN12", "KQ04", "KPN02", "KPN11")
 
 caps_sets <- read.table("caps_sets/wGRR_genomes_Twoway.txt")
 mat <- pivot_wider(caps_sets, names_from = V2, values_from = V3)
@@ -12,10 +12,10 @@ matrix <- mat[,-1]
 rownames(matrix) <- mat[,1]
 class(matrix) <- "numeric"
 mator <- matrix[order, order]
-colnames(mator) <- c("PF_EC19", "PF_EC22", "PF_EC12", "PF_EC07", "PF_EC02", "PF_EC03", "PF_EC10", "PF_EC21", "PF_EC16", "PF_EC25",
-                     "PF_KPN10", "PF_KPN14", "PF_KPN01", "PF_KPN16", "PF_KQ01", "PF_KPN09", "PF_KPN12", "PF_KPN02", "PF_KPN11", "PF_KQ04")
-rownames(mator) <- c("PF_EC19", "PF_EC22", "PF_EC12", "PF_EC07", "PF_EC02", "PF_EC03", "PF_EC10", "PF_EC21", "PF_EC16", "PF_EC25",
-                     "PF_KPN10", "PF_KPN14", "PF_KPN01", "PF_KPN16", "PF_KQ01", "PF_KPN09", "PF_KPN12", "PF_KPN02", "PF_KPN11", "PF_KQ04")
+colnames(mator) <- c("PF_EC22", "PF_EC12", "PF_EC07", "PF_EC02", "PF_EC19", "PF_EC03", "PF_EC10", "PF_EC21", "PF_EC16", "PF_EC25",
+                     "PF_KPN10", "PF_KPN01", "PF_KPN14", "PF_KPN16", "PF_KQ01", "PF_KPN09", "PF_KPN12", "PF_KQ04", "PF_KPN02", "PF_KPN11")
+rownames(mator) <- c("PF_EC22", "PF_EC12", "PF_EC07", "PF_EC02", "PF_EC19", "PF_EC03", "PF_EC10", "PF_EC21", "PF_EC16", "PF_EC25",
+                     "PF_KPN10", "PF_KPN01", "PF_KPN14", "PF_KPN16", "PF_KQ01", "PF_KPN09", "PF_KPN12", "PF_KQ04", "PF_KPN02", "PF_KPN11")
 
 
 
