c4084caae65b83d2018a4d0abad7c228ef0b8b0d
Laura Toribio-Celestino <105511747+LaboraTORIbio@users.noreply.github.com>
2023-08-11 15:51:09 +0200

    Updating previous files

--- experimental_figs_stats/figs_stats_mic_conj.R
+++ experimental_figs_stats/figs_stats_mic_conj.R
@@ -2,6 +2,7 @@
 library(tidyverse)
 library(ggpubr)
 library(readxl)
+library(ggplot2)
 
 #Figure 1A. MICs
 
@@ -102,4 +103,87 @@ pairwise.wilcox.test(CC_pairs$Value, CC_pairs$Species , paired = TRUE, alternati
 #Kruskal-wallis test for single and pool conjugations in vitro and in vivo conjugations
 
 kruskal.test(Value ~ interaction(Donor,Species_recipient), data = CC_pool %>% filter(Donor=="beta3914"))
+
+
+
+#Figs Supp AMC
+
+MIC_TC <- MICS[MICS$Type == "TC",]
+
+#Fold change MIC
+ggplot(MIC_TC, aes(x=Species, y=FC_AMC, fill=Species)) +
+  geom_boxplot(outlier.shape=NA, notch=F, show.legend = FALSE, lwd=1.2, width = 0.7) +
+  geom_point(position=position_jitter(width=0.2), alpha=0.45, shape=21, show.legend = FALSE, size = 3) +
+  theme_bw() + scale_fill_manual(values=c("#f4646dff", "#009ee3ff")) +
+  theme(text = element_text(size=16)) + theme(axis.text.x = element_text(size = 16)) + theme(axis.text.y = element_text(size = 14)) +
+  scale_x_discrete(labels=c("E_coli"="E. coli", "Klebsiella_spp"="Klebsiella spp.")) +
+  geom_signif(comparisons = list(c("E_coli", "Klebsiella_spp")), map_signif_level=TRUE, margin_top = 0.05, textsize = 5, vjust = 0.1, tip_length = 0)
+ggplot(MIC_TC, aes(x=Species, y=FC_ERT, fill=Species)) +
+  geom_boxplot(outlier.shape=NA, notch=F, show.legend = FALSE, lwd=1.2, width = 0.7) +
+  geom_point(position=position_jitter(width=0.2), alpha=0.45, shape=21, show.legend = FALSE, size = 3) +
+  theme_bw() + scale_fill_manual(values=c("#f4646dff", "#009ee3ff")) + 
+  theme(text = element_text(size=16)) + theme(axis.text.x = element_text(size = 16)) + theme(axis.text.y = element_text(size = 14)) +
+  scale_x_discrete(labels=c("E_coli"="E. coli", "Klebsiella_spp"="Klebsiella spp.")) +
+  geom_signif(comparisons = list(c("E_coli", "Klebsiella_spp")), map_signif_level=TRUE, margin_top = 0.05, textsize = 6, vjust = 0.6, tip_length = 0)
+ggplot(MIC_TC, aes(x=Species, y=FC_IMP, fill=Species)) +
+  geom_boxplot(outlier.shape=NA, notch=F, show.legend = FALSE, lwd=1.2, width = 0.7) +
+  geom_point(position=position_jitter(width=0.2), alpha=0.45, shape=21, show.legend = FALSE, size = 3) +
+  theme_bw() + scale_fill_manual(values=c("#f4646dff", "#009ee3ff")) + 
+  theme(text = element_text(size=16)) + theme(axis.text.x = element_text(size = 16)) + theme(axis.text.y = element_text(size = 14)) +
+  scale_x_discrete(labels=c("E_coli"="E. coli", "Klebsiella_spp"="Klebsiella spp.")) +
+  geom_signif(comparisons = list(c("E_coli", "Klebsiella_spp")), map_signif_level=TRUE, margin_top = 0.05, textsize = 5, vjust = 0.1, tip_length = 0)
+ggplot(MIC_TC, aes(x=Species, y=FC_MER, fill=Species)) +
+  geom_boxplot(outlier.shape=NA, notch=F, show.legend = FALSE, lwd=1.2, width = 0.7) +
+  geom_point(position=position_jitter(width=0.2), alpha=0.45, shape=21, show.legend = FALSE, size = 3) +
+  theme_bw() + scale_fill_manual(values=c("#f4646dff", "#009ee3ff")) + 
+  theme(text = element_text(size=16)) + theme(axis.text.x = element_text(size = 16)) + theme(axis.text.y = element_text(size = 14)) +
+  scale_x_discrete(labels=c("E_coli"="E. coli", "Klebsiella_spp"="Klebsiella spp.")) +
+  geom_signif(comparisons = list(c("E_coli", "Klebsiella_spp")), map_signif_level=TRUE, margin_top = 0.05, textsize = 5, vjust = 0.1, tip_length = 0)
+
+#Delta MIC
+ggplot(MIC_TC, aes(x=Species, y=delta_AMC, fill=Species)) +
+  geom_boxplot(outlier.shape=NA, notch=F, show.legend = FALSE, lwd=1.2, width = 0.7) +
+  geom_point(position=position_jitter(width=0.2), alpha=0.45, shape=21, show.legend = FALSE, size = 3) +
+  theme_bw() + scale_fill_manual(values=c("#f4646dff", "#009ee3ff")) + 
+  theme(text = element_text(size=16)) + theme(axis.text.x = element_text(size = 16)) + theme(axis.text.y = element_text(size = 14)) +
+  scale_x_discrete(labels=c("E_coli"="E. coli", "Klebsiella_spp"="Klebsiella spp.")) +
+  geom_signif(comparisons = list(c("E_coli", "Klebsiella_spp")), map_signif_level=TRUE, margin_top = 0.05, textsize = 6, vjust = 0.6, tip_length = 0)
+ggplot(MIC_TC, aes(x=Species, y=delta_ERT, fill=Species)) +
+  geom_boxplot(outlier.shape=NA, notch=F, show.legend = FALSE, lwd=1.2, width = 0.7) +
+  geom_point(position=position_jitter(width=0.2), alpha=0.45, shape=21, show.legend = FALSE, size = 3) +
+  theme_bw() + scale_fill_manual(values=c("#f4646dff", "#009ee3ff")) +
+  theme(text = element_text(size=16)) + theme(axis.text.x = element_text(size = 16)) + theme(axis.text.y = element_text(size = 14)) +
+  scale_x_discrete(labels=c("E_coli"="E. coli", "Klebsiella_spp"="Klebsiella spp.")) +
+  geom_signif(comparisons = list(c("E_coli", "Klebsiella_spp")), map_signif_level=TRUE, margin_top = 0.05, textsize = 5, vjust = 0.1, tip_length = 0)
+ggplot(MIC_TC, aes(x=Species, y=delta_IMP, fill=Species)) +
+  geom_boxplot(outlier.shape=NA, notch=F, show.legend = FALSE, lwd=1.2, width = 0.7) +
+  geom_point(position=position_jitter(width=0.2), alpha=0.45, shape=21, show.legend = FALSE, size = 3) +
+  theme_bw() + scale_fill_manual(values=c("#f4646dff", "#009ee3ff")) + 
+  theme(text = element_text(size=16)) + theme(axis.text.x = element_text(size = 16)) + theme(axis.text.y = element_text(size = 14)) +
+  scale_x_discrete(labels=c("E_coli"="E. coli", "Klebsiella_spp"="Klebsiella spp.")) +
+  geom_signif(comparisons = list(c("E_coli", "Klebsiella_spp")), map_signif_level=TRUE, margin_top = 0.05, textsize = 6, vjust = 0.6, tip_length = 0)
+ggplot(MIC_TC, aes(x=Species, y=delta_MER, fill=Species)) +
+  geom_boxplot(outlier.shape=NA, notch=F, show.legend = FALSE, lwd=1.2, width = 0.7) +
+  geom_point(position=position_jitter(width=0.2), alpha=0.45, shape=21, show.legend = FALSE, size = 3) +
+  theme_bw() + scale_fill_manual(values=c("#f4646dff", "#009ee3ff")) + 
+  theme(text = element_text(size=16)) + theme(axis.text.x = element_text(size = 16)) + theme(axis.text.y = element_text(size = 14)) +
+  scale_x_discrete(labels=c("E_coli"="E. coli", "Klebsiella_spp"="Klebsiella spp.")) +
+  geom_signif(comparisons = list(c("E_coli", "Klebsiella_spp")), map_signif_level=TRUE, margin_top = 0.05, textsize = 5, vjust = 0.1, tip_length = 0)
+
+
+#Conjugation rates
+CC_tasas<- read_excel("source_data.xlsx",
+                      sheet = "Conjugation rates (Fig. S14)")
+
+order <- c("PF_EC02", "PF_EC03", "PF_EC07", "PF_EC10", "PF_EC12", "PF_EC16", "PF_EC19", "PF_EC21", "PF_EC22", "PF_EC25",
+           "PF_KPN12", "PF_KQ01", "PF_KPN10", "PF_KPN11", "PF_KQ04", "PF_KPN16", "PF_KPN01", "PF_KPN02", "PF_KPN14", "PF_KPN09")
+CC_tasas$Recipient <- factor(CC_tasas$Recipient, levels=order)
+
+ggplot(CC_tasas, aes(x=Recipient, y=Value, color=Species, shape=Replicate)) +
+  geom_jitter(position=position_dodge(0.8)) +
+  scale_color_manual(values=c("#f4646dff", "#009fe3ff")) +
+  scale_shape_identity() +
+  scale_y_continuous(trans = 'log10') +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle=-45, vjust=1, hjust=0))
 
